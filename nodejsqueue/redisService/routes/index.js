4e6f28658bcb1367d4c4dadb1a1b5b79d124f92f
lichien2017 <lichien@qq.com>
2018-03-16 16:18:00 +0800

    1、增加mongodb模块 2、增加图片上传服务、通知数据上传服务、applist服务

--- nodejsqueue/redisService/routes/index.js
+++ nodejsqueue/redisService/routes/index.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var redis = require('redis');
+var mongodb = require('mongodb');
+var formidable = require('formidable');
+const path = require('path');
+const fs = require('fs');
 
 var router = express.Router();
 
-var redisIp = 'redisdb';
+var redisIp ='redisdb';//container里面的redisdb名称
+var mongodbIp = 'mongodb';//container里面的mongodb名称
 
-/* GET home page. */
+var DB_CONN_STR = 'mongodb://'+mongodbIp+':27017';
+/**
+ * 测试地址
+ */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
     try{
@@ -44,6 +52,118 @@ router.get('/', function(req, res, next) {
 });
 
 
+
+router.get('/applist',function (req,res) {
+    try{
+        var client  = redis.createClient('6379', redisIp);
+
+        client.on('connect', function() {
+            console.log('connected');
+            //先把数据存进去
+            // var dataList = [];
+            // var data = {};
+            // data.tag = 'snssdk';
+            // data.pkg = 'com.ss.android.article.news';
+            // dataList.push(data);
+            // client.set('applist',JSON.stringify(dataList),redis.print);
+            // var rulerString =  client.get('applist',redis.print);//得到规则字符串
+            var rulerString =  client.get('ruler',function (err,result) {
+                //得到规则字符串
+                res.send({"status": "success","data":result});
+            });
+            if (!rulerString){
+                res.send({"status": "error"});
+            }
+            // res.send({"status": "success","data":rulerString});
+        });
+
+        // redis 链接错误
+        client.on("error", function(error) {
+            console.log(error);
+        });
+
+    }catch (e){
+        res.send({"status": "error"});
+        console.log(e);
+    }
+});
+
+/**
+ * 接收上传图片
+ */
+router.post("/image",function (req,res) {
+    try{
+        var form = new formidable.IncomingForm();
+        form.encoding = 'utf-8';
+        form.uploadDir = path.join(process.cwd() + "/../uploads");
+        form.keepExtensions = true;//保留后缀
+        form.maxFieldsSize = 20 * 1024 * 1024;
+        //处理图片
+        form.parse(req, function (err, fields, files){
+            var filename = files.file.name
+            var nameArray = filename.split('.');
+            var type = nameArray[nameArray.length - 1];
+            var name = '';
+            for (var i = 0; i < nameArray.length - 1; i++) {
+                name = name + nameArray[i];
+            }
+            var date = new Date();
+            //var time = '_' + date.getFullYear() + "_" + date.getMonth() + "_" + date.getDay() + "_" + date.getHours() + "_" + date.getMinutes();
+            var avatarName = name  + '.' + type;
+            var newPath = form.uploadDir + "/" + avatarName;
+            fs.renameSync(files.file.path, newPath);  //重命名
+            res.send({"status": "success","url":"/uploads/"+avatarName});
+        })
+    }catch (e){
+        console.log(e)
+        res.send({"status": "error"});
+    }
+
+});
+
+
+
+/**
+ * 插入原始push消息数据
+ * @param db mongodb数据库
+ * @param callback 回调
+ */
+var insertPushData = function(db, insertData,callback) {
+    //连接到表
+    var collection = db.collection('push');
+    //插入数据
+    collection.insertOne(insertData,function (err,result) {
+        if(err)
+        {
+            console.log('Error:'+ err);
+            return;
+        }
+        callback(result);
+    });
+}
+/**
+ * 接收push通知
+ */
+router.post("/push",function (req,res) {
+    console.log("收到push消息："+req.body);
+    try{
+        var MongoClient = mongodb.MongoClient;
+        MongoClient.connect(DB_CONN_STR, function(err, db) {
+            console.log("mongodb连接成功！");
+            //将接收到的数据插入数据库
+            insertPushData(db.db('crawlnews'), req.body,function(result) {
+                console.log(result);
+                db.close();
+                res.json({"status": "success"});
+            });
+        });
+
+    }catch (e){
+        res.json({"status": "error"});
+        console.log(e);
+    }
+});
+
 /**
  * 获取规则文件
  */
@@ -54,8 +174,14 @@ router.get('/ruler', function (req, res) {
         client.on('connect', function() {
             console.log('connected');
             //先把数据存进去
-            var rulerString =  client.get('ruler',redis.print);//得到规则字符串
-            res.send({"status": "success","data":rulerString});
+            var rulerString =  client.get('ruler',function (err,result) {
+                //得到规则字符串
+                res.send({"status": "success","data":result});
+            });
+            if (!rulerString){
+                res.send({"status": "error"});
+            }
+
         });
 
         // redis 链接错误
