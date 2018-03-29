be1a6b72b4b093b75b4254ab32ca01c748aeb709
lichien2017 <lichien@qq.com>
2018-03-29 15:55:42 +0800

    kk

--- monkeyrunner/py2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mv Dockerfile_python2 Dockerfile
-docker build -t tymx/ecological:v1 .
--- monkeyrunner/py3.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mv Dockerfile_python3_adb Dockerfile
-docker build -t tymx/ecological:py3 .
