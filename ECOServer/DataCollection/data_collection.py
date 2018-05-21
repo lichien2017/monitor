8e534e1eb141eb476515f9b5ee52c8115b562cec
lichien2017 <lichien@qq.com>
2018-05-21 19:10:49 +0800

    修改数据汇总

--- ECOServer/DataCollection/data_collection.py
+++ ECOServer/DataCollection/data_collection.py
@@ -46,6 +46,67 @@ class Collector(Thread):
         conn.close()
         pass
 
+    def copy_data_to_total(self,table_name,rule_tag,date):
+        if table_name == None:
+            SingleLogger().log.debug("table_name is None")
+            return
+        table = self._database[table_name]
+        rows = table.find() # 查询出所有数据
+        conn = MySQLHelper.pool_connection.get_connection()
+        # 创建游标
+        cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
+        for row in rows :
+            res = self.get_resource(date, row["res_id"])
+            if res == None:
+                SingleLogger().log.error("resid = %s,没有找到详情信息，所以跳过"% row["res_id"])
+                continue
+            # insert_sql =
+            # 执行SQL，并返回收影响行数
+            try:
+
+                #result = cursor.fetchone()
+
+                row_count = cursor.execute("""select res_id from analysis_data_normal_total 
+                                  where res_id = '%s' and create_date = '%s'""" % (row["res_id"],date))
+                if row_count == 0 :#插入新的数据
+                    sql_str = """INSERT INTO `analysis_data_normal_total`
+                                    (`res_id`,
+                                    `title`,
+                                    `description`,
+                                    `crawl_time`,
+                                    `XueXingBaoLiRule`,
+                                    `screenshot`,
+                                    `screen_index`,
+                                    `app_tag`,
+                                    `category_tag`,
+                                    `shorturl`,
+                                    `create_date`,
+                                    `SexyRule`,
+                                    `PoliticalRule`,
+                                    `ZongJiaoRule`,
+                                    `BiaoTiDangRule`)
+                                    VALUES
+                                    ('%s','%s','%s','%s',%d,'%s',%d,'%s','%s','%s','%s',%d,%d,%d,%d)
+                                    """ % (
+                                    row["res_id"],pymysql.escape_string(res["title"]),pymysql.escape_string(res["description"]),res["crawltimestr"],0,'',-1,
+                                    res["app_tag"], res["category_tag"], res["shorturl"],date, 0,0,0,0)
+
+                    SingleLogger().log.debug(sql_str)
+                    row_count = cursor.execute(sql_str)
+                    pass
+                # 更新
+                row_count = cursor.execute("""update analysis_data_normal_total 
+                                                              set `%s` = 1 where res_id = '%s' and create_date = '%s'"""
+                                           % (rule_tag, row["res_id"], date))
+
+                conn.commit()
+                SingleLogger().log.debug(row)
+            except Exception as e:
+                SingleLogger().log.error(e)
+            pass
+        cursor.close()
+        conn.close()
+        pass
     def get_resource(self,date,res_id):
         res_table = self._database["originnews%s" % date]
         res = res_table.find_one({"identity": res_id})
@@ -67,31 +128,84 @@ class Collector(Thread):
                 res = self.get_resource(date,row["res_id"])
                 if res == None :
                     continue
-                # insert_sql =
-                # 执行SQL，并返回收影响行数,'%s'
-                # SingleLogger().log.debug("""insert into `analysis_data_total`(`res_id`,`create_date`,`rule_tag`,`screenshot`,
-                #                   `screen_index`,
-                #                   `app_tag`,`category_tag`,`deleted`,`title`,`description`,`shorturl`,`changed`,`crawl_time`)
-                #                         VALUES('%s','%s','%s','%s','%s','%s','%s',0,'%s','%s','%s',0,'%s')
-                #                         ON DUPLICATE Key UPDATE res_id = '%s'
-                #                      """ % (row["res_id"], date, rule_tag, row["image"],row["screen_index"],
-                #                             res["app_tag"],res["category_tag"],res["title"],res["description"],
-                #                             res["shorturl"],res["crawltimestr"],row["res_id"]))
-                row_count = cursor.execute("""insert into `analysis_data_total`(`res_id`,`create_date`,`rule_tag`,`screenshot`,
-                                  `screen_index`,
-                                  `app_tag`,`category_tag`,`deleted`,`title`,`description`,`shorturl`,`changed`,`crawl_time`)
-                                        VALUES('%s','%s','%s','%s','%s','%s','%s',0,'%s','%s','%s',0,'%s')  
-                                        ON DUPLICATE Key UPDATE res_id = '%s'
-                                     """ % (row["res_id"], date, rule_tag, row["image"],row["screen_index"],
-                                            res["app_tag"],res["category_tag"],pymysql.escape_string(res["title"]),pymysql.escape_string(res["description"]),
-                                            res["shorturl"],res["crawltimestr"],row["res_id"]))
-                conn.commit()
+                try:
+                    row_count = cursor.execute("""insert into `analysis_data_total`(`res_id`,`create_date`,`rule_tag`,`screenshot`,
+                                      `screen_index`,
+                                      `app_tag`,`category_tag`,`deleted`,`title`,`description`,`shorturl`,`changed`,`crawl_time`)
+                                            VALUES('%s','%s','%s','%s','%s','%s','%s',0,'%s','%s','%s',0,'%s')  
+                                            ON DUPLICATE Key UPDATE res_id = '%s'
+                                         """ % (row["res_id"], date, rule_tag, row["image"],row["screen_index"],
+                                                res["app_tag"],res["category_tag"],pymysql.escape_string(res["title"]),pymysql.escape_string(res["description"]),
+                                                res["shorturl"],res["crawltimestr"],row["res_id"]))
+                    conn.commit()
+                except Exception as e:
+                    SingleLogger().log.error(e)
             SingleLogger().log.debug(row)
             pass
         cursor.close()
         conn.close()
         pass
 
+    def copy_data_to_total_screen_orc(self,table_name,rule_tag,date):
+        if table_name == None:
+            SingleLogger().log.debug("table_name is None")
+            return
+
+        table = self._database[table_name]
+        rows = table.find()
+        conn = MySQLHelper.pool_connection.get_connection()
+        # 创建游标
+        cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
+        for row in rows:
+            if row["screen_index"] !=-1:
+                res = self.get_resource(date,row["res_id"])
+                if res == None :
+                    continue
+                try:
+                    row_count = cursor.execute("""select res_id 
+                                                  from analysis_data_normal_total 
+                                                  where res_id = '%s' and create_date = '%s'""" % (row["res_id"], date))
+                    if row_count == 0:  # 插入新的数据
+                        sql_str = """INSERT INTO `analysis_data_normal_total`
+                                                        (`res_id`,
+                                                        `title`,
+                                                        `description`,
+                                                        `crawl_time`,
+                                                        `XueXingBaoLiRule`,
+                                                        `screenshot`,
+                                                        `screen_index`,
+                                                        `app_tag`,
+                                                        `category_tag`,
+                                                        `shorturl`,
+                                                        `create_date`,
+                                                        `SexyRule`,
+                                                        `PoliticalRule`,
+                                                        `ZongJiaoRule`,
+                                                        `BiaoTiDangRule`)
+                                                        VALUES
+                                                        ('%s','%s','%s','%s',%d,'%s',%s,'%s','%s','%s','%s',%d,%d,%d,%d)
+                                                        """ % (
+                            row["res_id"], pymysql.escape_string(res["title"]), pymysql.escape_string(res["description"]),
+                            res["crawltimestr"], 0, row["image"],row["screen_index"],
+                            res["app_tag"], res["category_tag"], res["shorturl"], date, 0, 0, 0, 0)
+
+                        SingleLogger().log.debug(sql_str)
+                        row_count = cursor.execute(sql_str)
+                        pass
+                    # 更新
+                    row_count = cursor.execute("""update analysis_data_normal_total 
+                                                  set screenshot = '%s' ,screen_index = %s 
+                                                  where res_id = '%s' and create_date = '%s'"""
+                                               % (row["image"],row["screen_index"], row["res_id"], date))
+
+                    conn.commit()
+                except Exception as e:
+                    SingleLogger().log.error(e)
+            SingleLogger().log.debug(row)
+            pass
+        cursor.close()
+        conn.close()
+        pass
     #移除老的数据
     def remove_all_table_data(self,date):
         conn = MySQLHelper.pool_connection.get_connection()
@@ -102,6 +216,11 @@ class Collector(Thread):
                 delete from `analysis_data_total`
                 where create_date = '%s'
                 """ % (date))
+
+        row_count = cursor.execute("""
+                        delete from `analysis_data_normal_total`
+                        where create_date = '%s'
+                        """ % (date))
         cursor.close()
         conn.close()
         pass
@@ -146,7 +265,7 @@ select mongodb_tablename from `analysis_rules` where level = 0)
         result = cursor.fetchone()
         yestoday = LocalTime.from_today(self.time_go)
         yestoday_str = yestoday.strftime("%Y%m%d")
-        self.remove_all_table_data(yestoday_str)
+        self.remove_all_table_data(yestoday_str) # 清除当天数据
         while result != None:
             SingleLogger().log.debug(result)
             table_name = "%s%s" % (result["mongodb_tablename"],yestoday_str)
@@ -154,9 +273,14 @@ select mongodb_tablename from `analysis_rules` where level = 0)
 
             if result["mongodb_tablename"].startswith("screencapocr") : #如果是判断第几屏幕，单独处理
                 self.copy_data_screen_orc(table_name,result["mongodb_tablename"],yestoday_str)
+
+                self.copy_data_to_total_screen_orc(table_name, result["imp_python_class"], yestoday_str)
                 pass
             else: #其他规则直接存到指定的表格中
                 self.copy_data_one_column(table_name,result["mongodb_tablename"],yestoday_str)
+                level = int(result["level"])
+                if level == 1 :
+                    self.copy_data_to_total(table_name,result["imp_python_class"],yestoday_str)
                 pass
             result = cursor.fetchone()
         # 汇总所有的统计数据
