62f8edc2b81d67decdb65db5695f0f2413c644b3
lichien2017 <lichien@qq.com>
2018-05-28 22:16:37 +0800

    增加专题

--- ECOServer/mitmproxyServer/AddRule.py
+++ ECOServer/mitmproxyServer/AddRule.py
@@ -6,13 +6,17 @@ from datetime import datetime,timezone,timedelta
 import time
 import re
 
+redisdb = "redisdb"
 class RedisHelper():
-    connection_pool = redis.ConnectionPool(host="redisdb", port=6379, db=0)
+    connection_pool = redis.ConnectionPool(host=redisdb, port=6379, db=0)
     strict_redis = redis.StrictRedis(connection_pool=connection_pool)
 
 class AddRule:
     def __init__(self):
-        self.p = re.compile(r'.+?-channel', re.IGNORECASE)
+        rules_str = RedisHelper.strict_redis.get("rules").decode("utf-8")
+        print(rules_str)
+        self.rules = json.loads(rules_str)
+
         pass
 
 
@@ -26,9 +30,9 @@ class AddRule:
         # print(local_dt)
         return local_dt
         pass
-    def response(self, flow:http.HTTPFlow):
-        #print(flow.request.url)
-        if self.p.match(flow.request.url):
+    def match_url(self,tag,regex,flow:http.HTTPFlow):
+        array = regex.findall(flow.request.url)
+        if len(array)>0:
             print(flow.request.url)
             print("%s" % self.now().time())
             # print("postdata=%s"%flow.request.content.decode("utf-8"))
@@ -42,7 +46,22 @@ class AddRule:
             post_data["time"] = time.mktime(self.now().timetuple())
             json_str = json.dumps(post_data)
             print("json_str=%s" % json_str)
-            RedisHelper.strict_redis.lpush("origin:yiidan", json_str)
+            RedisHelper.strict_redis.lpush("origin:" + tag, json_str)
+            return True
+        else:
+            return False
+        pass
+    def response(self, flow:http.HTTPFlow):
+        # print(flow.request.url)
+        # p = re.compile(r"news.+?channel")
+        # self.match_url("ydsdk", p, flow)
+        for rule in self.rules :
+            #print(rule["regStr"])
+            p = re.compile(rule["regStr"], re.IGNORECASE)
+            if self.match_url(rule["tag"],p,flow):
+                break
+            pass
+
         #flow.response.headers["count"] = str(self.num)
 
 
@@ -52,7 +71,7 @@ addons = [
 
 
 if __name__ == "__main__":
-    p = re.compile(r'.+?-channel', re.IGNORECASE)
+    p = re.compile(r'channel/news-list-for.*channel')
     url = "https://124.243.231.139/Website//channel/news-list-for-hot-channel?searchentry=channel_navibar&reqid=b6fcpe5d_1527486704709_344033&eventid=6759219382e0a6acd-5a73-46b3-b754-49907be92c94&infinite=true&distribution=app.qq.com&refresh=1&appid=yidian&cstart=0&platform=1&cv=4.6.0.5&fields=docid&fields=date&fields=image&fields=image_urls&fields=like&fields=source&fields=title&fields=url&fields=comment_count&fields=up&fields=down&cend=30&version=020600&ad_version=010946&group_fromid=g181&collection_num=0&net=wifi"
-    rel = p.match(url)
+    rel = p.findall(url)
     pass
