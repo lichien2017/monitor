e619d363e5e9cf5da1f0adaa799f7c78846e1972
lichien2017 <lichien@qq.com>
2018-06-06 17:13:19 +0800

    修改生成训练数据脚本

--- importdata.sh
+++ importdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo '执行开始'
-docker run --rm --net=dockercontainer_mobilephone -v /home/tymx/Documents/DockerContainer/ECOServer:/usr/local/ECOServer -v /mnt/train_files:/tmp tymx/ecoserver:v2 python /usr/local/ECOServer/importtraindata.py
+docker run --rm --net=dockercontainer_mobilephone_release -v /home/tymx/Documents/DockerContainer/ECOServer:/usr/local/ECOServer -v /mnt/train_files:/tmp dockercontainer_ecoserver python /usr/local/ECOServer/importtraindata.py
 echo '结束'
