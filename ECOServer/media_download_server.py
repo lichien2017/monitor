e330d2691e71dbbbb6005547ff747efdfc71ae90
lichien2017 <lichien@qq.com>
2018-06-08 23:00:39 +0800

    下载服务可以添加多个线程来处理

--- ECOServer/media_download_server.py
+++ ECOServer/media_download_server.py
@@ -1,7 +1,9 @@
 
 from mediaDownloadServer.media_download import MediaDownload
 
-
+THREAD_COUNT = 1 # 启动多个线程来处理
 if __name__ == '__main__':
-    r = MediaDownload()
-    r.start()
+    for i in range(THREAD_COUNT): # 启动多个线程来处理
+        r = MediaDownload()
+        r.start()
+        pass
