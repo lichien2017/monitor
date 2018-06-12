4547aa07b40b2489e85f77528b2a6d3a3174fa4f
lichien2017 <lichien@qq.com>
2018-06-12 09:03:07 +0800

    新增两个重启服务脚本

---
+++ bash/restart_media_download.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker-compose -f /home/tymx/Documents/DockerContainer/docker-compose_distributed.yml restart imagedownload
---
+++ bash/restart_other_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker-compose -f /home/tymx/Documents/DockerContainer/docker-compose_distributed.yml restart mitmproxy
+docker-compose -f /home/tymx/Documents/DockerContainer/docker-compose_distributed.yml restart ecoserver
