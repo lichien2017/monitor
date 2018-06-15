658085fc950b9d570f026fb0f0a3cebf0723091e
lichien2017 <lichien@qq.com>
2018-06-15 17:05:43 +0800

    1、修改原始数据库入库模块，增加时间字段

--- ECOServer/scrapyServer/BaseModel.py
+++ ECOServer/scrapyServer/BaseModel.py
@@ -59,7 +59,7 @@ class BaseParse(object):
         # 把数据分发给打标服务，服务分为两类，一类基础服务（0），一类高级服务（1）
         # 数据包里面要包含标示和日期，所以要重新构建包 lzq
         msg = {"res_id":"%s"%articleid,"time":LocalTime.get_local_date(record_date_utc,"%Y-%m-%d %H:%M:%S").strftime("%Y%m%d"),
-               "record_time":sdata["crawltimestr"]}
+               "record_time":"%s" % sdata["crawltimestr"]}
         SingleLogger().log.debug("Rule0server.execute_all == %s",json.dumps(msg))
         Rule0server.execute_all(json.dumps(msg)) # 插入的数据格式为json
         # SingleLogger().log.debug("Rule1server.add_resource_to_all_queue == %s", json.dumps(msg))
