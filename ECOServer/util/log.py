62ee62bec530d8ad557a5d36c91acf60a61cac00
lichien2017 <lichien@qq.com>
2018-06-27 18:39:07 +0800

    去掉文件日志handler

--- ECOServer/util/log.py
+++ ECOServer/util/log.py
@@ -20,18 +20,18 @@ class Logger(logging.Logger):
     def __init__(self, filename=None):
         super(Logger, self).__init__(self)
         # 日志文件名
-        if filename is None:
-            filename = 'pt.log'
-
-        if not os.path.exists(self.BASE_PATH) :
-            os.makedirs(self.BASE_PATH)
-
-        self.filename = filename
+        # if filename is None:
+        #     filename = 'pt.log'
+        #
+        # if not os.path.exists(self.BASE_PATH) :
+        #     os.makedirs(self.BASE_PATH)
+        #
+        # self.filename = filename
 
         # 创建一个handler，用于写入日志文件 (每天生成1个，保留30天的日志)
-        fh = logging.handlers.TimedRotatingFileHandler(self.BASE_PATH +"/" + self.filename, 'D', 1, 30)
-        fh.suffix = "%Y%m%d-%H%M.log"
-        fh.setLevel(logging.DEBUG)
+        # fh = logging.handlers.TimedRotatingFileHandler(self.BASE_PATH +"/" + self.filename, 'D', 1, 30)
+        # fh.suffix = "%Y%m%d-%H%M.log"
+        # fh.setLevel(logging.DEBUG)
 
         # 再创建一个handler，用于输出到控制台
         ch = logging.StreamHandler()
@@ -39,11 +39,11 @@ class Logger(logging.Logger):
 
         # 定义handler的输出格式
         formatter = logging.Formatter('[%(asctime)s] - %(filename)s [Line:%(lineno)d] - [%(levelname)s]-[thread:%(thread)s]-[process:%(process)s] - %(message)s')
-        fh.setFormatter(formatter)
+        #fh.setFormatter(formatter)
         ch.setFormatter(formatter)
 
         # 给logger添加handler
-        self.addHandler(fh)
+        #self.addHandler(fh)
         self.addHandler(ch)
 
 class SingleLogger(Singleton):
