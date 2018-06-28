ba99bfaea6354a7ee89e7c0d149c30489767abb1
lichien2017 <lichien@qq.com>
2018-06-28 09:45:55 +0800

    修改编排任务

--- importdata.sh
+++ importdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo '执行开始'
-docker run --rm --net=dockercontainer_mobilephone_release -v /home/tymx/Documents/DockerContainer/ECOServer:/usr/local/ECOServer -v /mnt/train_files:/tmp dockercontainer_ecoserver python /usr/local/ECOServer/importtraindata.py
+docker run --rm --net=pub_mobilephone_release -v /home/tymx/Documents/DockerContainer/ECOServer:/usr/local/ECOServer -v /mnt/train_files:/tmp pub_ecoserver python /usr/local/ECOServer/importtraindata.py
 echo '结束'
